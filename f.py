543368b9bc1c979a88dd93b80aa5d042d3440967
abhayananda2002 <abhayanandapanda2002@gmail.com>
2024-08-16 10:13:17 +0530

    "arithmetic operator"

--- f.py
+++ f.py
@@ -1,7 +1,2 @@
-course= "python for beginners"
-#print (course.upper())
-#print (course.lower())
-#print (course.find("p"))
-#print (len(course))
-#print (course.replace("p" , "j"  ))
-'python' in course
+x=(2+3)*10-3
+print(x)
