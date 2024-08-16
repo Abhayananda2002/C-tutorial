8695c112efd53a551ef2a3f0be2e265dd0d0bcf0
abhayananda2002 <abhayanandapanda2002@gmail.com>
2024-08-16 10:37:55 +0530

    "if operator"

--- f.py
+++ f.py
@@ -1,2 +1,10 @@
-x=2.3
-print(round(x))
+is_hot=False
+is_cold=False
+if is_hot:
+    print("it is a hot day")
+    print("drink plenty of water")
+elif is_cold:
+    print("it is a cold day")
+else:
+    print("have a good day")
+print("enjoy your day")
