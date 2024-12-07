a2821aedb829eb8555d90cfb27593aa5976b4161
abhayananda2002 <abhayanandapanda2002@gmail.com>
2024-12-07 20:03:05 +0530

    "ABC BCD CDE"

--- Abhaya.cpp
+++ Abhaya.cpp
@@ -5,18 +5,18 @@ int main() {
     int n;
     cin >> n;
 
-    int i = 1; // Outer loop to control the number of rows
-    while (i <= n) {
-        char count = 'A'; // Initialize count to 'A' for every row
-        int j = 1;        // Use an integer to limit the loop to 3 characters
-        while (j <= n) {  // Inner loop runs exactly 3 times to print 'A', 'B', 'C'
-            cout << count << " ";
-            count++; // Move to the next character
-            j++;
-        }
-        cout << endl; // Move to the next row
-        i++;
+   int row=1;
+   while (row<=n) {
+    char ch='A';
+    int col=1;
+    while (col<=n) {
+        ch='A'+row+col-2;
+        cout<<ch<<" ";
+        col++;
     }
+    cout<<endl;
+    row++;
+   }
 
     return 0;
 }
