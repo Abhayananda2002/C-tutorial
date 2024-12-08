90b3ea57be5c852fa63c04dd590cbfa343ddafb3
abhayananda2002 <abhayanandapanda2002@gmail.com>
2024-12-08 15:36:49 +0530

    "print a 1 121 12321 1234321"

--- Abhaya.cpp
+++ Abhaya.cpp
@@ -6,10 +6,24 @@ int main() {
     cin >> n;
    int i=1;
   while(i<=n) {  
-    int col=1;
-     while (col<=n-i+1) {
-         cout <<"*";
+    //print space 
+    int space= n-i;
+    while(space){
+        cout<<" ";
+        space--;
+    }
+    //print 1st triangle
+     int col=1;
+     while (col<=i) {
+         cout <<col;
         col++;
+         }
+         //print 2nd triangle
+         int j=i-1;
+         while(j) 
+         {
+            cout<<j;
+            j--;
          }
         
     cout<<endl;
