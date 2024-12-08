0fa197e0ec51e43d2d936acf1d7fffe6179a80ae
abhayananda2002 <abhayanandapanda2002@gmail.com>
2024-12-08 22:38:22 +0530

    "fibonacci series"

--- Abhaya.cpp
+++ Abhaya.cpp
@@ -2,31 +2,19 @@
 using namespace std;
 
 int main() {
+    int n=10;
+    int a=0;
+    int b=1;
+   cout<<a<<" "<<b<<" ";
+    for(int i=1;i<=10;i++){
+        int nxtnumber=a+b;
+        cout<<nxtnumber<<" ";
+        a=b;
+        b=nxtnumber;
+
+     
+    }
     
-     int a=3;
-     int b=5;
-     cout<< "a&b"<<(a&b)<<endl;
-     cout<< "a|b"<<(a|b)<<endl;
-     cout<< "~a"<<~a<<endl;
-     cout<< "a^b"<<(a^b)<<endl;
-
-
-     cout<< (17<<2)<<endl;
-     cout<<(17<<1)<<endl;
-     cout<<(18>>2)<<endl;
-     cout<<(18>>1)<<endl;
-     int i=7;
-     cout<< (i++) <<endl;
-    cout<< (++i) <<endl;
-    cout<< (i--) <<endl;
-     cout<< (--i) <<endl;
-     int a,b=1;
-     a=10;
-     if(++a)
-       cout<<b;
-       else
-       cout<<++b;
-
     return 0;
 }
   
