43216dff78518ad1cf912d74a5e86291e6ccad0d
abhayananda2002 <abhayanandapanda2002@gmail.com>
2024-12-09 16:15:02 +0530

    "counting number of notes in the single number"

--- Calculator.cpp
+++ Calculator.cpp
@@ -3,31 +3,36 @@ using namespace std;
 
 int main() {
 
-int a,b;
+int a;
 cout<<"enter the value of a"<<endl;
 cin>>a;
-cout<<"enter the value of b"<<endl;
-cin>>b;
-char op;
-cout<<"enter the operation you want to perform"<<endl;
-cin>>op;
+int b,c,d,e;
+//cout<<"enter the value of b"<<endl;
+//cin>>b;
+int notes;
+cout<<"enter the notes you want to count which is used in given numbers"<<endl;
+cin>>notes;
 
-switch (op) {  
+switch (notes) {  
 
-    case '*':cout <<(a*b)<<endl;
-             break;
+    case 100:cout <<(b=a/100)<<endl;
+             //break;
 
-    case '+' :cout <<(a+b)<<endl;
-             break;
+    case 10 :cout <<(c=a%100)<<endl;
+             //break;
 
-    case '-':cout <<(a-b)<<endl;
+    case 50:cout <<(c/50)<<endl;
+              //break;
+    case 5:cout <<(d=c%50)<<endl;
+
+     case 20:cout <<(d/20)<<endl;
+
+    case 2:cout <<(e=d%20)<<endl;
+
+    case 1:cout <<(e/1)<<endl;
               break;
 
-    case '/':cout <<(a/b)<<endl;
-              break;
-
-    case '%': cout <<(a%b)<<endl;
-             break;  
+   
 
     default : cout<<"please enter the valid operation"<<endl;
 }
